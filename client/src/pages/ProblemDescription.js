bb5e533f315719d0c2eab0ffe51743c084bf0344
SashimiDaBest <soleilpham@gmail.com>
2024-10-26 15:56:14 -0400

    add more

--- client/src/pages/ProblemDescription.js
+++ client/src/pages/ProblemDescription.js
@@ -20,9 +20,18 @@ const ProblemDescription = () => {
             <h1 className="title">What is your problem?</h1>  
             <div className="warning">If you are in immediate danger, please call 911!</div> 
             <div className="map"> --- MAP --- </div>
-            <div className="map"> --- MAP --- </div>
-            <div className="map"> --- MAP --- </div>
-            
+            <form action="/action_page.php">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name"></input>
+                <br></br>
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email"></input>
+                <br></br>
+                <label for="issueDescription">Description:</label>
+                <input type="text" id="description" name="description"></input>
+                <br></br>
+                <input onClick={handleClick1} type="submit" value="Submit"></input>
+            </form>
             <button onClick={handleClick2}>Back</button>
         </div>
     );
--- client/src/pages/VictimMap.js
+++ client/src/pages/VictimMap.js
@@ -8,7 +8,11 @@ const VictimMap = () => {
     const navigate = useNavigate();
 
     const handleClick1 = () => {
-        navigate('/VictimMap'); 
+        // navigate('/VictimMap'); 
+    };
+
+    const handleClick2 = () => {
+        navigate('/'); 
     };
 
     return (
@@ -16,14 +20,20 @@ const VictimMap = () => {
             <h1 className="title">You are here</h1>  
             <div className="warning">If you are in immediate danger, please call 911!</div> 
             <div className="map"> --- MAP --- </div>
+
+            <div className="direction">Your Name</div> 
+            <div className="direction">Your Email</div> 
+            <div className="direction">Your Problem Description</div> 
+
+            <button onClick={handleClick1}>Send Help!</button>
+            {/* 
+                make button disapear 
+                add text saying help request sent 
+                show time elapsed after help requent sent 
+                add button "finished with problem"
+            */}
+            <button onClick={handleClick2}>Back</button>
             
-            <div className="direction">Please descripe your problem:</div> 
-            <form action="/action_page.php">
-                <label for="issueDescription">Description:</label>
-                <input type="text" id="fname" name="fname"></input>
-                <br></br>
-                <input onClick={handleClick1} type="submit" value="Submit"></input>
-            </form>
         </div>
     );
 };
