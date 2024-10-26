7ef45b5f02d587d7af9f5fe6f9d394c1391a9c9d
jonathanpratt5564 <pratt79@purdue.edu>
2024-10-26 18:28:35 -0400

    Update NoPeopleAvail.js
    made NoPeopleAvail use default chakra ui components

--- client/src/pages/NoPeopleAvail.js
+++ client/src/pages/NoPeopleAvail.js
@@ -1,13 +1,11 @@
 import React from "react";
-// import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 const NoPeopleAvail = ({ message = "There are no people who need help right now. Thanks though!" }) => {
     return (
-        <div>
-            <h1>
-                {message}
-            </h1>
-        </div>
+        <Box textAlign="center" p="4">
+            <Heading size="md">{message}</Heading>
+        </Box>
     );
 };
 
