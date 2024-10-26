0a793766d0119efbff034507c9c411cd79925c55
SashimiDaBest <soleilpham@gmail.com>
2024-10-26 15:23:37 -0400

    add buttons to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
 import '../App.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleClick1 = () => {
+        navigate('/ProblemIdentification'); 
+    };
+    const handleClick2 = () => {
+        navigate('/HeroSelection'); 
+    };
+
     return (
         <div>
             <h1 className="title">Do you need help?</h1>   
+            <div className="map"> --- MAP --- </div>
+            <button onClick={handleClick1}>Yes</button>
+            <button onClick={handleClick2}>No</button>
         </div>
     );
 };
