e2bfb49862514e5754bfb9e2992d09fbaac15488
jonathanpratt5564 <pratt79@purdue.edu>
2024-10-26 20:23:42 -0400

    Update ConfirmInfo.js
    added footer

--- client/src/pages/ConfirmInfo.js
+++ client/src/pages/ConfirmInfo.js
@@ -5,7 +5,8 @@ import {
     Heading,
     Text,
     VStack,
-    Button
+    Button,
+    Link
 } from "@chakra-ui/react";
 
 const ConfirmInfo = ({ userName, userEmail, problemDescription }) => {
@@ -17,48 +18,66 @@ const ConfirmInfo = ({ userName, userEmail, problemDescription }) => {
     };
 
     return (
-        <Box p="6" minH="100vh" textAlign="center">
-            <Heading size="lg" mb="4">Selection Confirmed!</Heading>
-            <Text fontSize="lg" mb="8">
-                Email notification sent to the selected user.
+        <Box display="flex" flexDirection="column" minHeight="100vh" p="6" textAlign="center">
+            <Box flex="1">
+                <Heading size="lg" mb="4">Selection Confirmed!</Heading>
+                <Text fontSize="lg" mb="8">
+                    Email notification sent to the selected user.
+                </Text>
+
+                <VStack align="stretch" spacing="4" maxW="600px" mx="auto">
+                    <Text fontSize="lg">
+                        <strong>User Name:</strong> {userName || "Loading..."}
+                    </Text>
+                    <Text fontSize="lg">
+                        <strong>User Email:</strong> {userEmail || "Loading..."}
+                    </Text>
+                    <Text fontSize="lg"><strong>Problem Description:</strong></Text>
+                    <Box
+                        p="4"
+                        border="1px solid"
+                        borderColor="gray.200"
+                        borderRadius="md"
+                        minH="100px"
+                        overflowY="auto"
+                        fontSize="md"
+                    >
+                        {problemDescription || "Loading..."}
+                    </Box>
+                </VStack>
+
+                <Heading size="md" mt="12" mb="4">
+                    Please select one of the following actions upon arrival:
+                </Heading>
+
+                <VStack spacing="4" maxW="600px" mx="auto" w="full">
+                    <Button onClick={handleButtonClick} w="full">
+                        User successfully helped
+                    </Button>
+                    <Button onClick={handleButtonClick} w="full">
+                        Unable to help user
+                    </Button>
+                    <Button onClick={handleButtonClick} w="full">
+                        User not found at location
+                    </Button>
+                </VStack>
+            </Box>
+
+            <Text fontSize="sm" color="gray.500" textAlign="center" mb="4">
+                Contributors:{" "}
+                <Link href="https://www.linkedin.com/in/jonnen-chong-22034620a/" isExternal color="blue.500" textDecoration="underline">
+                    Jonnen Chong
+                </Link>,{" "}
+                <Link href="https://www.linkedin.com/in/anthony-huang-171910321/" isExternal color="blue.500" textDecoration="underline">
+                    Anthony Huang
+                </Link>,{" "}
+                <Link href="https://www.linkedin.com/in/chan-nhu-pham-4876a127a/" isExternal color="blue.500" textDecoration="underline">
+                    Soleil Pham
+                </Link>, and{" "}
+                <Link href="https://www.linkedin.com/in/jonathan-pratt-1a1196286/" isExternal color="blue.500" textDecoration="underline">
+                    Jonathan Pratt
+                </Link> - 2024
             </Text>
-
-            <VStack align="stretch" spacing="4" maxW="600px" mx="auto">
-                <Text fontSize="lg">
-                    <strong>User Name:</strong> {userName || "Loading..."}
-                </Text>
-                <Text fontSize="lg">
-                    <strong>User Email:</strong> {userEmail || "Loading..."}
-                </Text>
-                <Text fontSize="lg"><strong>Problem Description:</strong></Text>
-                <Box
-                    p="4"
-                    border="1px solid"
-                    borderColor="gray.200"
-                    borderRadius="md"
-                    minH="100px"
-                    overflowY="auto"
-                    fontSize="md"
-                >
-                    {problemDescription || "Loading..."}
-                </Box>
-            </VStack>
-
-            <Heading size="md" mt="12" mb="4">
-                Please select one of the following actions upon arrival:
-            </Heading>
-
-            <VStack spacing="4" maxW="600px" mx="auto" w="full">
-                <Button onClick={handleButtonClick} w="full">
-                    User successfully helped
-                </Button>
-                <Button onClick={handleButtonClick} w="full">
-                    Unable to help user
-                </Button>
-                <Button onClick={handleButtonClick} w="full">
-                    User not found at location
-                </Button>
-            </VStack>
         </Box>
     );
 };
