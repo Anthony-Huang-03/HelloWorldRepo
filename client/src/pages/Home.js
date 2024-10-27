6b0ff3646bd80801578824f8d31fa94c418fd469
jonathanpratt5564 <pratt79@purdue.edu>
2024-10-26 20:17:44 -0400

    Update Home.js
    add footer with links to linkedins

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Button, Heading, Text, VStack, Center } from "@chakra-ui/react";
+import { Box, Button, Heading, Text, VStack, Center, Link } from "@chakra-ui/react";
 import Map from "../components/Map";
 import axios from "axios";
 
@@ -44,21 +44,39 @@ const Home = () => {
     }, []);
 
     return (
-        <Box p="4" textAlign="center">
-            <Heading size="xl" mb="4">Do you need help?</Heading>
+        <Box display="flex" flexDirection="column" minHeight="100vh" p="4" textAlign="center">
+            <Box flex="1">
+                <Heading size="xl" mb="4">Do you need help?</Heading>
 
-            <Center mb="4">
-                {location.latitude && location.longitude && victims ? (
-                    <Map position={location} victims={victims} /> // Render the Map with the current location
-                ) : (
-                    <Text>Loading map...</Text>
-                )}
-            </Center>
+                <Center mb="4">
+                    {location.latitude && location.longitude && victims ? (
+                        <Map position={location} victims={victims} /> // Render the Map with the current location
+                    ) : (
+                        <Text>Loading map...</Text>
+                    )}
+                </Center>
 
-            <VStack spacing="4">
-                <Button onClick={handleClick1}>Yes</Button>
-                <Button onClick={handleClick2}>No</Button>
-            </VStack>
+                <VStack spacing="4">
+                    <Button onClick={handleClick1}>Yes</Button>
+                    <Button onClick={handleClick2}>No</Button>
+                </VStack>
+            </Box>
+
+            <Text fontSize="sm" color="gray.500" textAlign="center" mb="4">
+                Contributors:{" "}
+                <Link href="https://www.linkedin.com/in/jonnen-chong-22034620a/" isExternal color="blue.500" textDecoration="underline">
+                    Jonnen Chong
+                </Link>,{" "}
+                <Link href="https://www.linkedin.com/in/anthony-huang-171910321/" isExternal color="blue.500" textDecoration="underline">
+                    Anthony Huang
+                </Link>,{" "}
+                <Link href="https://www.linkedin.com/in/chan-nhu-pham-4876a127a/" isExternal color="blue.500" textDecoration="underline">
+                    Soleil Pham
+                </Link>, and{" "}
+                <Link href="https://www.linkedin.com/in/jonathan-pratt-1a1196286/" isExternal color="blue.500" textDecoration="underline">
+                    Jonathan Pratt
+                </Link> - 2024
+            </Text>
         </Box>
     );
 };
