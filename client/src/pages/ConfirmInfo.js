8f21f0e5cd47efe5a82cd6ca3239cdd8cd7077c4
jonathanpratt5564 <jonathanpratt5564@gmail.com>
2024-10-27 01:50:11 -0400

    footer component

--- client/src/pages/ConfirmInfo.js
+++ client/src/pages/ConfirmInfo.js
@@ -13,7 +13,7 @@ import {
 } from "@chakra-ui/react";
 import Background from "../assets/Background.jpeg"; // Import your background image
 import Logo from "../assets/roadieRescueLogo.png"; // Import the logo
-
+import Footer from "../components/Footer"
 const ConfirmInfo = ({ userName, userEmail, problemDescription }) => {
     const navigate = useNavigate();
 
@@ -96,33 +96,9 @@ const ConfirmInfo = ({ userName, userEmail, problemDescription }) => {
             </Box>
 
             {/* Contributors Box */}
-            <Box
-                border="1px"
-                borderColor="gray.200"
-                borderRadius="2xl"
-                p="4"
-                mt="6"
-                textAlign="center"
-                bg="white" // Fully opaque white background
-                mx="auto" // Center horizontally
-                width="600px" // Match the width of the main content box
-            >
-                <Text fontSize="sm" color="gray.500">
-                    Contributors:{" "}
-                    <Link href="https://www.linkedin.com/in/jonnen-chong-22034620a/" isExternal color="blue.500" textDecoration="underline">
-                        Jonnen Chong
-                    </Link>,{" "}
-                    <Link href="https://www.linkedin.com/in/anthony-huang-171910321/" isExternal color="blue.500" textDecoration="underline">
-                        Anthony Huang
-                    </Link>,{" "}
-                    <Link href="https://www.linkedin.com/in/chan-nhu-pham-4876a127a/" isExternal color="blue.500" textDecoration="underline">
-                        Soleil Pham
-                    </Link>, and{" "}
-                    <Link href="https://www.linkedin.com/in/jonathan-pratt-1a1196286/" isExternal color="blue.500" textDecoration="underline">
-                        Jonathan Pratt
-                    </Link> - 2024
-                </Text>
-            </Box>
+            <Footer>
+
+            </Footer>
         </Flex>
     );
 };
